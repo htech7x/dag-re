d99b8c42d086c57d3e24e56d400539fb9ca36331
Oleksandr Liemieshko <htech7x@gmail.com>
2025-08-04 16:45:12 -0500

    Create message.py

---
+++ message.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def print_hello():
+    print(" Hello Alex! ")
+
+
+with DAG(
+    dag_id="simple_hello_dag",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,  # Run on demand
+    catchup=False,
+    tags=["print hello"],
+) as dag:
+
+    hello_task = PythonOperator(
+        task_id="say_hello",
+        python_callable=print_hello,
+    )
